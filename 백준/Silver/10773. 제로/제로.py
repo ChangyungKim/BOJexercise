8d27a7b4e397249199863fa0d4bcc92366df1a5f
ChangyungKim <102973718+ChangyungKim@users.noreply.github.com>
2022-09-11 02:32:27 +0900

    [Silver IV] Title: 제로, Time: 120 ms, Memory: 31620 KB -BaekjoonHub

---
+++ 백준/Silver/10773. 제로/제로.py
@@ -0,0 +1,10 @@
+import sys
+K=int(sys.stdin.readline().rstrip())
+stack=[]
+for i in range(K):
+    num=int(sys.stdin.readline().rstrip())
+    if num==0:
+        stack.pop()
+    else:
+        stack.append(num)
+print(sum(stack))
