fb9257a02cbd75d79b3aa6a15090c844debd68d0
ChangyungKim <102973718+ChangyungKim@users.noreply.github.com>
2022-09-18 00:09:04 +0900

    [Silver I] Title: 오르막 수, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/11057. 오르막 수/오르막 수.py
@@ -0,0 +1,12 @@
+import sys
+N=int(sys.stdin.readline().rstrip())
+dp=[[0 for _ in range(10)] for _ in range(1001)]
+for i in range(10):
+    dp[1][i]=1
+for i in range(2,N+1):
+    for j in range(10):
+        if j==0:
+            dp[i][j]=1
+        else:
+            dp[i][j]=(dp[i-1][j]+dp[i][j-1])
+print(sum(dp[N])%10007)
