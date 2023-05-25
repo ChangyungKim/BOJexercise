f210549fcba3367ba7d86598be35e16e69f780a9
ChangyungKim <102973718+ChangyungKim@users.noreply.github.com>
2023-05-26 02:44:55 +0900

    [Bronze V] Title: A/B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+int main() {
+   double A,B;
+   cin>>A>>B;
+   double result=A/B;
+
+   cout.precision(30);
+   cout<<fixed;
+   cout<<result;
+
+}
