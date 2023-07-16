a6133c5c1bd08b70ab4652913524069df674e587
ChangyungKim <102973718+ChangyungKim@users.noreply.github.com>
2023-07-16 23:56:58 +0900

    [Silver II] Title: 약수의 합 2, Time: 212 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/17427. 약수의 합 2/약수의 합 2.py
+++ 백준/Silver/17427. 약수의 합 2/약수의 합 2.py
@@ -1,7 +1,6 @@
-n = int(input())
-
-sum = 0
-
-for i in range(1, n+1) :
-  sum += (n//i)*i
+import sys
+N=int(sys.stdin.readline().rstrip())
+sum=0
+for i in range(1,N+1):
+    sum+=(N//i)*i
 print(sum)
