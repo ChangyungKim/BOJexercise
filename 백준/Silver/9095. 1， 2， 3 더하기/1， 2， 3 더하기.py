4e2b54bfc8b766d2e1116880113c436d06369342
ChangyungKim <102973718+ChangyungKim@users.noreply.github.com>
2023-08-02 18:53:06 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
@@ -1,16 +1,11 @@
 import sys
 T=int(sys.stdin.readline().rstrip())
-result=[]
-lst=[0]*11
-lst[1]=1
-lst[2]=2
-lst[3]=4
+dp=[0]*11
+dp[1]=1
+dp[2]=2
+dp[3]=4
 for i in range(T):
     n=int(sys.stdin.readline().rstrip())
     for j in range(4, n+1):
-        lst[j]=lst[j-1]+lst[j-2]+lst[j-3]
-    result.append(lst[n])
-    
-for j in range(len(result)):
-    print(result[j])
-               
+        dp[j]=dp[j-1]+dp[j-2]+dp[j-3]
+    print(dp[n])
